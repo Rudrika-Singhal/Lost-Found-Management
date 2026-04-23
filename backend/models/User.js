b8c001771825ab9b5bc488dad597d2a605f2d2fb
Rudrika Singhal <rudrikasinghal05@gmail.com>
2026-04-23 12:02:04 +0530

    fixed User model structure

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
   name: String,
   email: { type: String, unique: true },
   password: String
 });
 
-module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model("User", userSchema);
